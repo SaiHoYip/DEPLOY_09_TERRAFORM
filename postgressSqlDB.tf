fec2fa9f3f8e7f10db7bd980469bc47a2db7c8ab
SaiHoYip <78388916+SaiHoYip@users.noreply.github.com>
2021-12-10 13:15:19 -0500

    Update postgressSqlDB.tf

--- postgressSqlDB.tf
+++ postgressSqlDB.tf
@@ -1,8 +1,23 @@
+resource "aws_security_group" "db_SG" {
+  name        = "db_SG"
+  description = "security group for load balancer"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TCP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [aws_security_group.allow_tcp.id]
+  }
+}
+
 resource "aws_db_instance" "default" {
   allocated_storage    = 20
   engine               = "postgressSQL"
   engine_version       = "9.6.20-R1"
   instance_class       = "db.t3.micro"
+  vpc_security_group_ids = [aws_security_group.db_SG]
   name                 = "mydb"
   username             = "sai"
   password             = "abc12345"
