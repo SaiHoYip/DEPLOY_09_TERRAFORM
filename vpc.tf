e12696022f8383bc4d20b8f08cfa6f7c479f4679
SaiHoYip <78388916+SaiHoYip@users.noreply.github.com>
2022-01-07 20:06:47 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -90,6 +90,7 @@ resource "aws_route_table" "Private-route-table" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.example.id
   }
 
   tags = {
