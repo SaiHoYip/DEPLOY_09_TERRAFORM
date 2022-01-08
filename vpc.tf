d946236af2bca9b2c3a5dcaaa34c0a15e36c1002
SaiHoYip <78388916+SaiHoYip@users.noreply.github.com>
2022-01-07 21:28:11 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -43,7 +43,7 @@ resource "aws_subnet" "Private01" {
 resource "aws_subnet" "internal01" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.4.0/24"
-  availability_zone       = "us-east-1b"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
