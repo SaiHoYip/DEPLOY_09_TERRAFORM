2f7782ffe6c38f62b227c7b7a9fe7e63c46982dc
SaiHoYip <78388916+SaiHoYip@users.noreply.github.com>
2022-01-07 20:40:36 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "allow_tcp" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [aws_security_group.lb_SG]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
